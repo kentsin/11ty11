2b7b9abbc718db1371d1f4f37b48155a3e200994
kentsin <kentsin@yahoo.com>
2021-03-24 12:42:53 +0800

    Create isotape.js

---
+++ src/site/isotape.js
@@ -0,0 +1,93 @@
+    // quick search regex
+    var qsRegex;
+    var buttonFilter;
+
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows',
+        filter: function () {
+            var $this = $(this);
+            var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+            var buttonResult = buttonFilter ? $this.is(buttonFilter) : true;
+            return searchResult && buttonResult;
+        },
+        getSortData: {
+            name: '.name',
+            symbol: '.symbol',
+            number: '.number parseInt',
+            category: '[data-category]',
+            weight: function (itemElem) {
+                var weight = $(itemElem).find('.weight').text();
+                return parseFloat(weight.replace(/[\(\)]/g, ''));
+            }
+        }
+    });
+
+    // filter functions
+    var filterFns = {
+        // show if number is greater than 50
+        numberGreaterThan50: function () {
+            var number = $(this).find('.number').text();
+            return parseInt(number, 10) > 50;
+        },
+        // show if name ends with -ium
+        ium: function () {
+            var name = $(this).find('.name').text();
+            return name.match(/ium$/);
+        }
+    };
+
+    // change is-checked class on buttons
+    $('.button-group').each(function (i, buttonGroup) {
+        var $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'button', function () {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
+    });
+
+    $('#filters').on('click', 'button', function () {
+        buttonFilter = $(this).attr('data-filter');
+        $grid.isotope();
+    });
+
+    // use value of search field to filter
+    var $quicksearch = $('#quicksearch').keyup(debounce(function () {
+        qsRegex = new RegExp($quicksearch.val(), 'gi');
+        $grid.isotope();
+    }));
+
+
+    // change is-checked class on buttons
+    $('.button-group').each(function (i, buttonGroup) {
+        var $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'button', function () {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
+    });
+
+    // bind sort button click
+    $('#sorts').on('click', 'button', function () {
+        var sortByValue = $(this).attr('data-sort-by');
+        $grid.isotope({
+            sortBy: sortByValue
+        });
+    });
+
+    // debounce so filtering doesn't happen every millisecond
+    function debounce(fn, threshold) {
+        var timeout;
+        return function debounced() {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+
+            function delayed() {
+                fn();
+                timeout = null;
+            }
+            setTimeout(delayed, threshold || 100);
+        };
+    }
